da5e38ab99e63be3f426e1a307a339bb70a0ef94
seydimouhamed <seydinabasse@gmail.com>
2020-11-16 14:02:09 +0000

    heroku

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
